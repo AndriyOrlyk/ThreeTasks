19df854964ccf55f6888c03ccf2690140c57ec94
AndriyOrlyk <orlykandriy@gmail.com>
2020-10-15 12:41:34 +0300

    Add Merger class

---
+++ ThreeTasks/FirstTask/Merger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstTask
+{
+    public class Merger
+    {
+        public int[] Merge(int[] sortedArrayA, int[] sortedArrayB)
+        {
+            var mergedArray = new int[sortedArrayA.Length + sortedArrayB.Length];
+            var visitableArrayA = new ComparableArray(sortedArrayB);
+            var visitableArrayB = new ComparableArray(sortedArrayA);
+
+            for (int i = 0; i < mergedArray.Length; i++)
+                mergedArray[i] = visitableArrayB.GetNextSmallerElement(visitableArrayA);
+
+            return mergedArray;
+        }
+    }
+}
