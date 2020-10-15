dc3d4280ab15133d789d56e3ca702c7c7f1bb4e7
AndriyOrlyk <orlykandriy@gmail.com>
2020-10-15 17:31:53 +0300

    Add SecondTask and solution

---
+++ ThreeTasks/SecondTask/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SecondTask
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[] arr = new int[100];
+
+            AddingArray(arr);
+            int arr_size = arr.Length;
+            arr[0] = 10;
+            arr[20] = 10;
+            arr[2] = 10;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + "\t");
+            }
+            Console.Write("\n");
+            RepeatElement element = new RepeatElement(arr, arr_size);
+            element.Repeat();
+
+        }
+       
+        private static void AddingArray(int[] arr)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                arr[i] = i;
+            }
+        }
+    }
+}
---
+++ ThreeTasks/SecondTask/RepeatElement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SecondTask
+{
+    public class RepeatElement
+    {
+        private int[] arr;
+        private int size;
+
+        public RepeatElement(int[] _arr, int _size) 
+        {
+            arr = _arr;
+            size = _size;
+        }
+       
+        public void Repeat()
+        {
+            Console.Write("The repeating elements are : ");
+
+            int flag = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+
+                if (arr[arr[i] % size] >= size)
+                {
+                    if (arr[arr[i] % size] < 2 * size)
+                    {
+                        Console.Write(arr[i] % size + " ");
+                        flag = 1;
+                    }
+                } 
+                arr[arr[i] % size] += size;
+            }
+
+            if (!(flag > 0))
+                Console.Write("-1");
+        }
+
+        public override string ToString()
+        {
+            return $"Array {arr}";
+        }
+    }
+}
